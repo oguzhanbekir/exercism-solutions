043e1143e638b7d8b3cf91a772865db82a86d3a7
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2021-05-26 01:48:13 +0300

    etl

---
+++ etl/Sources/Etl/Etl.swift
@@ -0,0 +1,13 @@
+//Solution goes in Sources
+
+import Foundation
+
+struct ETL {
+    static func transform(_ dict: [Int: [String]]) -> [String: Int] {
+        let transformed = dict.flatMap { key, value in
+            return value.flatMap { [$0.lowercased(): key] }
+        }
+
+        return Dictionary(uniqueKeysWithValues: transformed)
+    }
+}
