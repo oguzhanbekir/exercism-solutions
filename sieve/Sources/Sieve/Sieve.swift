e8a7ff320c8fb4b62f4695707c9fd4b843011c63
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2021-05-28 02:59:38 +0300

    sieve

---
+++ sieve/Sources/Sieve/Sieve.swift
@@ -0,0 +1,30 @@
+//Solution goes in Sources
+
+struct Sieve {
+    private(set) var primes: [Int] = []
+
+    init(_ limit: Int) {
+        self.primes = self.eratosthenes(limit)
+    }
+
+    private func eratosthenes(_ limit: Int) -> [Int] {
+        var isPrime = Array(repeating: true, count: limit + 1)
+        isPrime[0] = false
+        isPrime[1] = false
+        var result = Array<Int>()
+
+        for i in 2...limit {
+            if isPrime[i] {
+                result.append(i)
+
+                var j = i * i
+                while j <= limit {
+                    isPrime[j] = false
+                    j += i * (i == 2 ? 1 : 2)
+                }
+            }
+        }
+        return result
+    }
+}
+
