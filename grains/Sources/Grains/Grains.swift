af82cb0f7608742458af2834033fa9d05d480f4e
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2021-05-29 13:29:56 +0300

    grains

--- grains/Sources/Grains/Grains.swift
+++ grains/Sources/Grains/Grains.swift
@@ -3,18 +3,21 @@
 import Foundation
 
 struct Grains {
+    static var total = UInt64.max
+    static func square(_ input: Int) throws -> UInt {
+        guard case 1...64 = input else { throw(GrainsError(input)) }
+        let result = UInt(pow(2.0, Double(input - 1)))
+        return result
+    }
+}
+
+extension Grains {
     enum GrainsError: Error {
         case inputTooHigh(String)
         case inputTooLow(String)
-        init(_ val: Int) {
-            let message = "Input[\(val)] invalid. Input should be between 1 and 64 (inclusive)"
-            self = val > 64 ? .inputTooHigh(message) : .inputTooLow(message)
+        init(_ input: Int) {
+            let message = "Input[\(input)] invalid. Input should be between 1 and 64 (inclusive)"
+            self = input <= 0 ? .inputTooLow(message) : .inputTooHigh(message)
         }
     }
-    
-    static func square(_ input: Int) throws -> uint64 {
-        guard case 1...64 = input else { throw GrainsError(input) }
-        return uint64(1) << uint64(input - 1)
-    }
-    static let total = uint64.max
 }
